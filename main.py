81847e8b0dbd452d99086aa5c10a89289e1712ad
Yunus Emre Çelikkıran <yunise.yc@gmail.com>
2026-07-16 13:02:18 +0300

    Stage 5: Swagger UI

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, Response
 from pydantic import BaseModel
 
 
@@ -9,6 +9,11 @@ tasks = [
     {"id": 2, "title": "Task", "done": True},
     {"id": 3, "title": "Backend", "done": False}
 ]
+
+class Task(BaseModel):
+    title: str
+    done: bool = False
+    id: int = None
 
 @app.get("/")
 async def root():
@@ -27,17 +32,40 @@ def get_task(task_id: int):
     for task in tasks:
         if task["id"] == task_id:
             return task
-    return {"error": "Task not found"}
+    raise HTTPException(status_code=404, detail=f"Task {task_id} not found")
 
 @app.post("/tasks")
-def create_task(task: dict):
-    if not task.get("title"):
-        return {"error": status.HTTP_400_BAD_REQUEST, "message": "Title is required"}
+def create_task(task: Task):
+    if not task.title.strip():
+        raise HTTPException(status_code=400, detail="Title is required")
     
+    next_id = max([task["id"] for task in tasks]) + 1 if tasks else 1
     new_task = {
-        "id": len(tasks) + 1,
-        "title": task.get("title", ""),
-        "done": task.get("done", False)
+        "id": next_id,
+        "title": task.title,
+        "done": task.done
     }
     tasks.append(new_task)
     return new_task
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, task: Task):
+    if task.title is not None and not task.title.strip():
+        raise HTTPException(status_code=400, detail="Title cannot be empty or whitespaces only")
+    
+    for index, item in enumerate(tasks):
+        if item["id"] is None:
+            raise HTTPException(status_code=400, detail="Task ID is required")
+        if item["id"] == task_id:
+            tasks[index]["title"] = task.title
+            tasks[index]["done"] = task.done
+            return tasks[index]
+    raise HTTPException(status_code=404, detail=f"Task {task_id} not found")
+
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int):
+    for index, item in enumerate(tasks):
+        if item["id"] == task_id:
+            tasks.pop(index)
+            return Response(status_code=status.HTTP_204_NO_CONTENT)
+    raise HTTPException(status_code=404, detail=f"Task {task_id} not found")
